29c7b061102b42feb159f49bcd37523a923e8674
Valeria Esquivel <esq22001@byui.edu>
2024-07-22 23:28:47 -0600

    Page added to vite

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
         main: resolve(__dirname, "src/index.html"),
         spells: resolve(__dirname, "src/pages/spells.html"),
         houseCharacters: resolve(__dirname, "src/pages/houseCharacters.html"),
+        information: resolve(__dirname, "src/pages/information.html"),
       },
     },
   },
